d694b5b432ea6f6eade735a0cc7ea00c67cc0030
mmihira <mmihira.w@gmail.com>
2018-10-29 16:38:12 +1100

    Fix extensions typo

--- app/lib/api/extensions.js
+++ app/lib/api/extensions.js
@@ -2,6 +2,6 @@ const BASE_URL = '/api/v1';
 
 module.exports = {
   NAMESPACES: () => BASE_URL + '/namespaces',
-  NAMESPACE: (nSpace) => BASE_URL + `/namespaces/${nSpace}`
+  NAMESPACE: (nSpace) => BASE_URL + `/namespaces/${nSpace}`,
   FUNCTION: (nSpace, fName) => BASE_URL + `/namespaces/${nSpace}/${fName}`
 };
