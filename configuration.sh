63c92404a7fcfbfb0e75b36c0e7a7f9f0e11c3ea
Luca Morandini <lmorandini@ieee.org>
2018-11-15 08:49:08 +1100

    - Dropped use of env variable PGFAAS_BASE_URL

--- configuration.sh
+++ configuration.sh
@@ -3,7 +3,7 @@
 export EXPRESS_PORT=8071
 export EXPRESS_BIND_ADDRESS="0.0.0.0"
 
-export PGFAAS_URL_BASE="http://pgfaas.aurin.org.au"
+export PGFAAS_API_URL="http://pgfaas.aurin.org.au"
 
 export DOCKER_REGISTRY="lmorandini"
 export PGFAAS_UI_VERSION="latest"
